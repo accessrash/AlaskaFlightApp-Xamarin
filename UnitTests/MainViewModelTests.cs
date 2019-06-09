b9f5b3f6f9143bd2fff8373df6e2eef8902e9908
Rashmi Ramachandra <accessrash@gmail.com>
2019-06-08 18:17:25 -0700

    Added Unit test project and some unit tests for MainViewModel

---
+++ UnitTests/MainViewModelTests.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using AlaskaFlightApp.Core.Models;
+using AlaskaFlightApp.Core.ViewModels;
+using Moq;
+using NUnit.Framework;
+using UnitTests;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests: UnitTestBase
+    {
+
+        MainViewModel Subject { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            Subject = new MainViewModel(flightDataServiceMock.Object, connectionServiceMock.Object, dialogServiceMock.Object, mvaNavigationMock.Object);
+            Subject.keyboard = keyBoardMock.Object;
+        }
+
+        [Test]
+        public void ClickSearchCallsCheckOnline()
+        {
+            Subject.SearchCommand.Execute();
+            connectionServiceMock.Verify(x => x.CheckOnline(), Times.Once);
+        }
+
+        [Test]
+        public void ClickSearchCallsDismissKeyboard()
+        {
+            Subject.SearchCommand.Execute();
+            keyBoardMock.Verify(x => x.dismissKeyboard(), Times.Once);
+        }
+
+        [Test]
+        public void ClickSearchCallsGetFlightDetailsWhenOnline()
+        {
+            connectionServiceMock.Setup(x => x.CheckOnline()).Returns(true);
+            flightDataServiceMock.Setup(x => x.GetFlightDetails(It.IsAny<string>())).Returns(Task.FromResult(new List<FlightModel>()));
+            Subject.SearchCommand.Execute();
+            flightDataServiceMock.Verify(x => x.GetFlightDetails(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void ClickSearchCallsShowAlertAsyncWhenOnlineAndSearchResultIsEmpty()
+        {
+            connectionServiceMock.Setup(x => x.CheckOnline()).Returns(true);
+            flightDataServiceMock.Setup(x => x.GetFlightDetails(It.IsAny<string>())).Returns(Task.FromResult(new List<FlightModel>()));
+            Subject.SearchCommand.Execute();
+            dialogServiceMock.Verify(x => x.ShowAlertAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void ClickSearchCallsShowAlertAsyncWhenConnectionIsFalse()
+        {
+            connectionServiceMock.Setup(x => x.CheckOnline()).Returns(false);
+            Subject.SearchCommand.Execute();
+            dialogServiceMock.Verify(x => x.ShowAlertAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
---
+++ UnitTests/UnitTestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AlaskaFlightApp.Core.Contracts.Interactions;
+using AlaskaFlightApp.Core.Contracts.Service;
+using Moq;
+using MvvmCross.Navigation;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class UnitTestBase
+    {
+        protected Mock<IKeyboard> keyBoardMock { get; set; }
+        protected Mock<IFlightDataService> flightDataServiceMock { get; set; }
+        protected Mock<IConnectionService> connectionServiceMock { get; set; }
+        protected Mock<IDialogService> dialogServiceMock { get; set; }
+        protected Mock<IMvxNavigationService> mvaNavigationMock { get; set; }
+
+
+        [SetUp] //Called before every test run
+        public void UnitTestBaseSetUp()
+        {
+            keyBoardMock = new Mock<IKeyboard>();
+            flightDataServiceMock = new Mock<IFlightDataService>();
+            connectionServiceMock = new Mock<IConnectionService>();
+            dialogServiceMock = new Mock<IDialogService>();
+            mvaNavigationMock = new Mock<IMvxNavigationService>();
+        }
+    }
+}
